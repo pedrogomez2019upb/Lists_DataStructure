87422ba9669f00b5ccafc0d7453c795ef9f9ef88
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-07-29 13:01:45 -0500

    Fixing ListNode and List

--- src/edu/upb/ed/lists/List.java
+++ src/edu/upb/ed/lists/List.java
@@ -8,61 +8,83 @@ public class List {
     public ListNode tail;
     public int size;
 
-    public List(){
+    public List() {
         head = null;
-        tail =null;
-        size=0;
+        tail = null;
+        size = 0;
 
     }
 
-    public List(Object object)
+    public Object get(int index)
     {
-        //add(object);
+        int counter=0;
+        ListNode temporalNode=head;
+        while (counter<index){
+            temporalNode=temporalNode.getNext();
+            counter++;
+        }
+        return temporalNode.getObject();
     }
 
-    public void insert(int position,Object object){
-        if (position >= 0 && position<=size){
+
+    public void insert(int position, Object object) {
+        if (position >= 0 && position <= size) {
             ListNode newNode = new ListNode();
             newNode.setObject(object);
-            ListNode aux=head;
-            for(int i=0;i<(position-1);i++)
-            {
-                aux=aux.getNext();
+            ListNode aux = head;
+            for (int i = 0; i < (position - 1); i++) {
+                aux = aux.getNext();
             }
-            ListNode nNext=aux.getNext();
+            ListNode nNext = aux.getNext();
             aux.setNext(newNode);
             newNode.setNext(nNext);
             size++;
         }
     }
 
-    public void removeTail(int position)
-    {
-        ListNode aux=head;
-        for(int i=0;i<position-1;i++)
-        {
-            aux=aux.getNext();
+    public void removeTail(int position) {
+        ListNode aux = head;
+        for (int i = 0; i < position - 1; i++) {
+            aux = aux.getNext();
         }
-        ListNode nNext=aux.getNext();
+        ListNode nNext = aux.getNext();
         aux.setNext(nNext.getNext());
         size--;
     }
 
-    public void add(int position){
-
+    public void add(Object object)
+    {
+        if(head==null){
+            head = new ListNode(object);
+            tail = head;
+        }
+        else
+        {
+           ListNode temporalNode=head;
+           ListNode newNode=new ListNode(object);
+           newNode.linkNext(temporalNode);
+           head=newNode;
+        }
+        size ++;
     }
 
-    /*
+
     public List(Object object){
         head= new ListNode(object);
         tail =head;
     }
 
-     */
+    public void seeList(){
+        if(!isEmpty())
+        {
+            ListNode aux=head;
+        }
+    }
+
     public boolean isEmpty(){
         return head == null;
     }
-
+/*
     @Override
     public Iterator<ListNode> iterator()
     {
@@ -77,6 +99,12 @@ public class List {
             public ListNode next() {
                 return null;
             }
-        }
+
+        };
+
+
+     */
+
     }
-}
+
+
